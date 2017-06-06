04b173b97e17c1896dfbc61dbc89a608a852c502
Onaulogho Lawrence <onaulogholawrence@gmail.com>
2017-06-06 17:15:46 +0100

    Second change

--- collection/admin.py
+++ collection/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+# Import your model
+from collection.models import Profile
+
+# Set up automated slug creation
+class ProfileAdmin(admin.ModelAdmin):
+    model = Profile
+    # We want this to display on the admin dashboard
+    list_display = ('name', 'description',)
+    prepopulated_fields = {'slug': ('name',)}
 
 # Register your models here.
+admin.site.register(Profile, ProfileAdmin)
